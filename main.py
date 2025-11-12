edc8c20daa87153e2cb0d7c4cceacf8fe37d7413
Jarn de Jong <jarndejong@gmail.com>
2025-11-12 12:53:26 +0100

    save to disk handling with wrapper

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 #%%
-
 from settings import settings
 from src.sync import sync
-from src.save import save_unmatched, save_matched
+from src.save import handle_savetodisk
 
+#%%
 matched, unmatched, plex_tracks = sync(settings)
 
 print(f"Number of unmatched tracks: {len(unmatched)}")
 
-if settings['print_unmatched_to_file']:
-    save_unmatched(unmatched = unmatched, 
-                   settings = settings)
-
-if settings['print_matched_to_file']:
-    save_matched(matched = matched,
-                 found = plex_tracks,
-                 settings = settings)
-
+handle_savetodisk(unmatched, matched, plex_tracks, settings)
